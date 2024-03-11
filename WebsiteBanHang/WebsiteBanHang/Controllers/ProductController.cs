53cbe1bd6f67e82590bf48c723e31c920f8d5a37
DannyCandy <dankbzzz01@gmail.com>
2024-03-11 21:50:52 +0700

    BE only

---
+++ WebsiteBanHang/WebsiteBanHang/Controllers/ProductController.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using WebsiteBanHang.Models; // Thay thế bằng namespace thực tế của bạn
+using WebsiteBanHang.Repositories; // Thay thế bằng namespace thực tế của bạn
+
+namespace WebsiteBanHang.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _productRepository;
+
+        public ProductController(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Product product, IFormFile imageUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (imageUrl != null)
+                {
+                    // Lưu hình ảnh đại diện
+                    product.ImageUrl = await SaveImage(imageUrl);
+                }
+                _productRepository.Add(product);
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        private async Task<string> SaveImage(IFormFile image)
+        {
+            var savePath = Path.Combine("wwwroot/images", image.FileName); // Thay đổi đường dẫn theo cấu hình của bạn
+            using (var fileStream = new FileStream(savePath, FileMode.Create))
+            {
+                await image.CopyToAsync(fileStream);
+            }
+            return "/images/" + image.FileName; // Trả về đường dẫn tương đối
+        }
+
+
+        // Các actions khác như Display, Update, Delete
+
+        // Display a list of products
+        public IActionResult Index()
+        {
+            var products = _productRepository.GetAll();
+            if(products.Any())
+            {
+                return View(products);
+            }
+            return RedirectToAction("EmptyPage");
+        }
+
+        // Display a single product
+        public IActionResult Display(int id)
+        {
+            var product = _productRepository.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // Show the product update form
+        public IActionResult Update(int id)
+        {
+            var product = _productRepository.GetById(id);
+            if (product is null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // Process the product update
+
+        [HttpPost]
+        public async Task<IActionResult> Update(Product product, IFormFile imageUrl)
+        {
+            if (ModelState.IsValid)
+            {
+                if (imageUrl != null)
+                {
+                    // Lưu hình ảnh đại diện
+                    product.ImageUrl = await SaveImage(imageUrl);
+                }
+
+                _productRepository.Update(product);
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        // Show the product delete confirmation
+        public IActionResult Delete(int id)
+        {
+            var product = _productRepository.GetById(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return View(product);
+        }
+
+        // Process the product deletion
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            _productRepository.Delete(id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult EmptyPage()
+        {
+            return View();
+        }
+
+    }
+
+}
+
---
+++ WebsiteBanHang/WebsiteBanHang/Repositories/MockProductRepository.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebsiteBanHang.Models; // Thay thế bằng namespace thực tế của bạn
+namespace WebsiteBanHang.Repositories
+{
+    public class MockProductRepository : IProductRepository
+    {
+        private List<Product> _products = new List<Product>();
+
+        public MockProductRepository()
+        {
+            // Tạo một số dữ liệu mẫu
+            
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public Product GetById(int id)
+        {
+            return _products.FirstOrDefault(p => p.Id == id);
+        }
+
+        public void Add(Product product)
+        {
+            if(_products.Any())
+            {
+                product.Id = _products.Max(p => p.Id) + 1;
+                _products.Add(product);
+            }
+            else
+            {
+                product.Id = 1;
+                _products.Add(product);
+            }
+        }
+
+        public void Update(Product product)
+        {
+            var index = _products.FindIndex(p => p.Id == product.Id);
+            if (index != -1)
+            {
+                _products[index] = product;
+            }
+        }
+
+        public void Delete(int id)
+        {
+            var product = _products.FirstOrDefault(p => p.Id == id);
+            if (product != null)
+            {
+                _products.Remove(product);
+            }
+        }
+
+    }
+}
